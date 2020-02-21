cc8144aa21c0c2c2a21382f8ff819c4f2e573fda
Ryan Scott Lewis <ryanscottlewis@gmail.com>
2020-02-21 18:14:31 -0500

    App: Add alias for Delta

--- src/stag.cr
+++ src/stag.cr
@@ -3,8 +3,11 @@ module Stag
   VERSION = "0.0.1"
 
   alias Arguments       = Array(String)
-  alias FilesystemEntry = NamedTuple(type: File::Type, path: String)
+
   alias Query           = Crecto::Repo::Query
+
+  alias FilesystemEntry = NamedTuple(type: File::Type, path: String)
+  alias FilesystemDelta = NamedTuple(creation: Array(FilesystemEntry), deletion: Array(FilesystemEntry))
 
 end
 
