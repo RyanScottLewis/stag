cb8740bb22a909174c28dabf6fd29d9331320be9
Ryan Scott Lewis <ryanscottlewis@gmail.com>
2020-02-20 22:03:43 -0500

    App: GenerateVirtualManifest - Refactor large method into helper methods

--- src/stag/operation/filesystem/generate_virtual_manifest.cr
+++ src/stag/operation/filesystem/generate_virtual_manifest.cr
@@ -23,22 +23,34 @@ class Stag::Operation::Filesystem::GenerateVirtualManifest < Stag::Operation::Ba
   end
 
   protected def generate_tag_entry(tag : Model::Tag, memo = [] of FilesystemEntry)
-    memo << { type: File::Type::Directory, path: File.join(@options.root, tag.path!) }
+    create_directory_entry(tag, memo)
 
-    # TODO: Figure out preloading because this defeats the purpose of associations...
-    query = Query.join(:unions).where("unions.tag_id = ?", tag.id)
-    sources = Repository.all(Model::Source, query)
+    sources  = retrieve_sources(tag)
+    children = retrieve_children(tag)
 
-    query = Query.where(parent_id: tag.id)
-    children = Repository.all(Model::Tag, query)
-
-    sources.each do |source|
-      memo << { type: File::Type::Symlink, path: File.join(@options.root, tag.path!, source.path!) }
-    end
-
-    children.each { |child| generate_tag_entry(child, memo) }
+    sources.each  { |source| create_symlink_entry(tag, source, memo) }
+    children.each { |child|  generate_tag_entry(child, memo) }
 
     memo
+  end
+
+  # TODO: Figure out preloading because this defeats the purpose of associations...
+  protected def retrieve_sources(tag)
+    query = Query.join(:unions).where("unions.tag_id = ?", tag.id)
+    Repository.all(Model::Source, query)
+  end
+
+  protected def retrieve_children(tag)
+    query = Query.where(parent_id: tag.id)
+    Repository.all(Model::Tag, query)
+  end
+
+  protected def create_directory_entry(tag, memo)
+    memo << { type: File::Type::Directory, path: File.join(@options.root, tag.path!) }
+  end
+
+  protected def create_symlink_entry(tag, source, memo)
+    memo << { type: File::Type::Symlink, path: File.join(@options.root, tag.path!, source.path!) }
   end
 
 end
