069a9619610794c3f82fb4b8e0163df0a0580ee6
Ryan Scott Lewis <ryanscottlewis@gmail.com>
2020-02-24 15:32:39 -0500

    App: Workaround for Query.preload(:tags) giving unexpected results

--- src/stag/action/index.cr
+++ src/stag/action/index.cr
@@ -1,8 +1,17 @@
 class Stag::Action::Index < Stag::Action::Base
 
   def call
-    query   = Query.preload([:unions, :tags])
-    sources = Repository.all(Model::Source, query)
+    Crecto::DbLogger.set_handler(STDOUT)
+
+    sources = Repository.all(Model::Source, Query.preload(:source_tags))
+
+    # TODO: This works but it should have *just worked* with Query.preload(:tags)
+    sources.each do |source|
+      query = Query.preload(:source_tags)
+      source.tags = source.source_tags.map do |source_tag|
+        Repository.get(Model::Tag, source_tag.tag_id)
+      end.compact
+    end
 
     puts Formatter::Index::Table.call(sources)
   end
