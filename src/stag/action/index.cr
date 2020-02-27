152def9df281f42374111a690cf9a87bade7b198
Ryan Scott Lewis <ryanscottlewis@gmail.com>
2020-02-27 04:50:35 -0500

    App: Index action refactor

--- src/stag/action/index.cr
+++ src/stag/action/index.cr
@@ -1,24 +1,21 @@
 class Stag::Action::Index < Stag::Action::Base
 
-  @sources        = [] of Model::Source
-  @action_options = Options::Index.new
-
   def call
     parse_options
-    retrieve_sources
-    print_table
+    sources = retrieve_sources
+    print_table(sources)
   end
 
   protected def parse_options
-    Operation::ParseOptions::Index.call(@arguments, @action_options, OptionParser.new) # TODO: NOT THIS but here to compile
+    OptionParser::Index.call(@cli)
   end
 
   protected def retrieve_sources
-    @sources = Operation::RetrieveSources.call
+    Operation::RetrieveSources.call
   end
 
-  protected def print_table
-    puts Formatter::Index::Table.call(@action_options, @sources)
+  protected def print_table(sources)
+    puts Formatter::Index::Table.call(@cli.options[:index], sources)
   end
 
 end
