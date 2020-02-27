ed5ff2b72f51ed9e8a78369a6667a8de46dbee79
Ryan Scott Lewis <ryanscottlewis@gmail.com>
2020-02-27 00:00:56 -0500

    App: Global options use HOME environment variable

--- src/stag/options/global.cr
+++ src/stag/options/global.cr
@@ -4,8 +4,8 @@ class Stag::Options::Global < Stag::Options::Base
   property help     = false
   property verbose  = false
   property dry      = false
-  property root     = "~/.local/share/stag/fs"
-  property database = "~/.local/share/stag/database.db"
+  property root     = "#{ENV["HOME"]}/.local/share/stag/fs"
+  property database = "#{ENV["HOME"]}/.local/share/stag/database.db"
 
 end
 
