385e357739ce861df8213bf7369a9e9dcc55bbfd
Ryan Scott Lewis <ryanscottlewis@gmail.com>
2020-02-29 22:00:51 -0500

    App: Help action works for global and index

--- src/stag/action/help.cr
+++ src/stag/action/help.cr
@@ -1,7 +1,15 @@
 class Stag::Action::Help < Stag::Action::Base
 
   def call
-    puts self.class
+    action_name = (@cli.arguments.first? || "global").strip.downcase
+
+    # TODO: There is where the router helper function would go... IF I HAD ONE
+    key = case action_name
+    when "index", "list" then :index
+    else; :global
+    end
+
+    puts @cli.option_parsers[key]
   end
 
 end
