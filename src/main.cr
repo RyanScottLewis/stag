6af088e6e8d86bdf6f08204e64fc4be9c2431aa8
Ryan Scott Lewis <ryanscottlewis@gmail.com>
2020-03-01 00:49:17 -0500

    Use personal formatter for Crecto output

--- src/main.cr
+++ src/main.cr
@@ -28,6 +28,17 @@ require "./stag/interface/**"
 
 require "./stag/application"
 
+
+def Crecto::DbLogger.log(string, elapsed) : Nil
+  @@log_handler.as(IO) << [
+    "  DB  ".colorize(:blue),
+    ("%7.7s" % elapsed_text(elapsed)).colorize(:dark_gray),
+    string,
+    "\n"
+  ].join(" ")
+end
+
+
 # Init
 Stag::Application.call(ARGV)
 
